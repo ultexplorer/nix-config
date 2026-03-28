5eff6cb306a1951601cc107b5491fcc6548d71bc
Igor <ultexplorer@gmail.com>
2026-03-28 22:26:53 +0000

    fix boot.nix

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -1,21 +1,32 @@
 { pkgs, ... }:
 
 {
- # boot.kernelPackages = pkgs.linuxPackages_latest;
+  # boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.kernelParams = [ "quiet" "splash" "loglevel=3" ];
+  # fbcon=nodefer защищает крупный шрифт от сброса при загрузке видеодрайвера AMD
+  boot.kernelParams = [ "quiet" "splash" "loglevel=3" "fbcon=nodefer" ];
 
   # Настройки консоли (TTY)
   console = {
     enable = true;
-    font = "ter-v32n"; # Крупный шрифт (Terminus)
+    font = "ter-v32n"; 
     packages = [ pkgs.terminus_font ];
-    
-    keyMap = "us";     # Оставляем английскую раскладку по умолчанию
+    earlySetup = true; # Шрифт появится сразу при вводе пароля/загрузке
+    keyMap = "us"; 
   };
 
-  # Системный язык (можешь оставить ru или поставить en)
-  i18n.defaultLocale = "ru_RU.UTF-8";
+  # Гарантируем наличие шрифта в системе
+  i18n.consolePackages = [ pkgs.terminus_font ];
+
+  # Устанавливаем системный язык на английский (США)
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Если всё же нужны русские форматы (даты, валюта), можно добавить это:
+  # i18n.extraLocaleSettings = {
+  #   LC_TIME = "ru_RU.UTF-8";
+  #   LC_MONETARY = "ru_RU.UTF-8";
+  # };
 }
+
