31589578c871f4916b4e840aa1a61942efe16d3b
Igor <ultexplorer@gmail.com>
2026-03-28 22:08:28 +0000

    fix kernelPackages

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 {
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+ # boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
